8748b93aab78a7e1fe491e9c6ee2be0f379f9732
Alex <zhouliang106@qq.com>
2021-02-06 01:28:52 +0800

    update

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,13 @@ import ComponentIcon from './icon.vue'
 import ComponentColorfulText from './colorful-text.vue'
 import ComponentViewPage from './view-page.vue'
 import ComponentViewSection from './view-section.vue'
+import ComponentTextColumns from './text-columns.vue'
 
 export default [
   ComponentImage,
   ComponentIcon,
   ComponentColorfulText,
   ComponentViewPage,
-  ComponentViewSection
+  ComponentViewSection,
+  ComponentTextColumns
 ]
