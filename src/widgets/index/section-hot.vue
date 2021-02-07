9aefcc3019db2ac2d810f140d08b37d915cbc85a
Alex <zhouliang106@qq.com>
2021-02-08 06:11:49 +0800

    update style for desktop

--- src/widgets/index/section-hot.vue
+++ src/widgets/index/section-hot.vue
@@ -2,45 +2,47 @@
   <!-- 国际热点 有轮播 -->
   <al-section class="widget-index-section-hot">
     <div class="hot-wrapper">
-      <p class="hot-city-en">{{ activeSwiper.en }}</p>
-      <p class="hot-city-title">{{ activeSwiper.title }}</p>
-      <p class="hot-city-en-description">{{ activeSwiper.enDescription }}</p>
-      <p class="hot-city-split-text">{{ activeSwiper.splitText }}</p>
-      <div class="description-wrapper">
-        <p
-          class="cn-description-item"
-          v-for="(item, i) in activeSwiper.description"
-          :key="i"
-        >
-          {{ item }}
-        </p>
-      </div>
-      <div class="swiper-dots">
-        <span
-          class="swiper-dot"
-          :class="{ active: j === activeIndex + 1 }"
-          v-for="j in hotDetail.length"
-          :key="j"
-        ></span>
-      </div>
-      <div class="hot-swiper-control">
-        <al-icon
-          name="arrowGray"
-          position="left"
-          @click="handleSwiperNext(-1)"
-        ></al-icon>
-        <al-icon
-          name="arrowGray"
-          position="right"
-          @click="handleSwiperNext(1)"
-        ></al-icon>
+      <div class="hot-context">
+        <p class="hot-city-en">{{ activeSwiper.en }}</p>
+        <p class="hot-city-title">{{ activeSwiper.title }}</p>
+        <p class="hot-city-en-description">{{ activeSwiper.enDescription }}</p>
+        <p class="hot-city-split-text">{{ activeSwiper.splitText }}</p>
+        <div class="description-wrapper">
+          <p
+            class="cn-description-item"
+            v-for="(item, i) in activeSwiper.description"
+            :key="i"
+          >
+            {{ item }}
+          </p>
+        </div>
+        <div class="swiper-dots">
+          <span
+            class="swiper-dot"
+            :class="{ active: j === activeIndex + 1 }"
+            v-for="j in hotDetail.length"
+            :key="j"
+          ></span>
+        </div>
+        <div class="hot-swiper-control">
+          <al-icon
+            name="arrowGray"
+            position="left"
+            @click="handleSwiperNext(-1)"
+          ></al-icon>
+          <al-icon
+            name="arrowGray"
+            position="right"
+            @click="handleSwiperNext(1)"
+          ></al-icon>
+        </div>
       </div>
       <div class="swiper-round-active">0{{ activeIndex + 1 }}</div>
       <div class="swiper-item-a">
-        <al-image :src="activeSwiper.imageB"></al-image>
+        <al-image :src="activeSwiper.imageA"></al-image>
       </div>
       <div class="swiper-item-b">
-        <al-image :src="activeSwiper.imageA"></al-image>
+        <al-image :src="activeSwiper.imageB"></al-image>
       </div>
     </div>
     <al-image class="layout-mobile-only" :src="activeSwiper.mobile"></al-image>
@@ -58,7 +60,7 @@ export default {
       hotDetail: [
         {
           mobile: "./images/hot-1.png",
-          desktop: "./images/section-hot-pc.png",
+          desktop: "./images/section-hot-pc-1.png",
           title: "我没去东京。",
           subtitle: "",
           en: "Tokyo.",
@@ -73,7 +75,7 @@ export default {
         },
         {
           mobile: "./images/hot-2.png",
-          desktop: "./images/section-hot-pc.png",
+          desktop: "./images/section-hot-pc-2.png",
           title: "我没去巴黎。",
           subtitle: "",
           en: "Tokyo.",
@@ -88,7 +90,7 @@ export default {
         },
         {
           mobile: "./images/hot-3.png",
-          desktop: "./images/section-hot-pc.png",
+          desktop: "./images/section-hot-pc-3.png",
           title: "我没去米兰。",
           subtitle: "",
           en: "Tokyo.",
@@ -103,7 +105,7 @@ export default {
         },
         {
           mobile: "./images/hot-4.png",
-          desktop: "./images/section-hot-pc.png",
+          desktop: "./images/section-hot-pc-4.png",
           title: "我没去巴塞罗那。",
           subtitle: "",
           en: "Tokyo.",
@@ -118,6 +120,15 @@ export default {
         },
       ],
     };
+  },
+  mounted() {
+    if (!this.$isMobile) {
+      this.hotDetail = this.hotDetail.map((item, i) => {
+        item.imageA = `./images/hot-${i + 1}-a-pc.jpg`;
+        item.imageB = `./images/hot-${i + 1}-b-pc.jpg`;
+        return item;
+      });
+    }
   },
   computed: {
     activeSwiper() {
@@ -161,20 +172,20 @@ export default {
   }
   .swiper-item-a {
     position: absolute;
-    z-index: 10;
-    left: vw(90);
-    bottom: vw(260);
-    width: vw(570);
+    z-index: 9;
+    right: vw(40);
+    bottom: vw(480);
+    width: vw(320);
     height: vw(320);
     border-radius: vw(20);
     overflow: hidden;
   }
   .swiper-item-b {
     position: absolute;
-    z-index: 9;
-    right: vw(40);
-    bottom: vw(480);
-    width: vw(320);
+    z-index: 10;
+    left: vw(90);
+    bottom: vw(260);
+    width: vw(570);
     height: vw(320);
     border-radius: vw(20);
     overflow: hidden;
@@ -250,6 +261,88 @@ export default {
 
   @include layout-desktop-full {
     margin-top: px2vw(-178);
+    .hot-wrapper {
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      padding: 0;
+    }
+    .hot-context {
+      position: absolute;
+      width: px2vw(450);
+      top: px2vw(100);
+      left: px2vw(210);
+    }
+    .hot-city-en {
+      font-size: px2vw(14);
+    }
+    .hot-city-title {
+      font-size: px2vw(42);
+      line-height: px2vw(62);
+    }
+    .hot-city-en-description {
+      margin-top: px2vw(12);
+      font-size: px2vw(14);
+    }
+    .hot-city-split-text {
+      margin-top: px2vw(60);
+      padding-left: px2vw(52);
+      font-size: px2vw(18);
+      &::before {
+        content: "";
+        top: px2vw(9);
+        width: px2vw(40);
+      }
+    }
+    .cn-description-item {
+      max-width: 100%;
+      margin-top: px2vw(20);
+      line-height: px2vw(22);
+      font-size: px2vw(14);
+    }
+
+    .hot-swiper-control {
+      .component-icon {
+        width: px2vw(66);
+        height: px2vw(60);
+        opacity: 0.1;
+      }
+    }
+
+    .swiper-item-a {
+      right: px2vw(210);
+      bottom: px2vw(250);
+      width: px2vw(320);
+      height: px2vw(320);
+      border-radius: 0;
+    }
+
+    .swiper-item-b {
+      left: auto;
+      right: px2vw(620);
+      bottom: px2vw(345);
+      width: px2vw(570);
+      height: px2vw(320);
+      border-radius: 0;
+    }
+    .swiper-dots {
+      margin: px2vw(50) 0;
+      width: px2vw(100);
+    }
+    .swiper-dot {
+      width: px2vw(8);
+      height: px2vw(8);
+    }
+    .swiper-round-active {
+      top: px2vw(270);
+      right: auto;
+      left: px2vw(580);
+      width: px2vw(64);
+      height: px2vw(64);
+      font-size: px2vw(14);
+      line-height: px2vw(64);
+    }
   }
 }
 </style>
