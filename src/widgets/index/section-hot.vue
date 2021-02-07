13946834c63a36d52fd4e8e374bd04c25e3c9f63
Alex <zhouliang106@qq.com>
2021-02-08 05:26:47 +0800

    update mobile swiper

--- src/widgets/index/section-hot.vue
+++ src/widgets/index/section-hot.vue
@@ -1,19 +1,253 @@
 <template>
   <!-- 国际热点 有轮播 -->
   <al-section class="widget-index-section-hot">
-    <al-image
-      class="layout-mobile-only"
-      src="./images/section-hot.png"
-    ></al-image>
+    <div class="hot-wrapper">
+      <p class="hot-city-en">{{ activeSwiper.en }}</p>
+      <p class="hot-city-title">{{ activeSwiper.title }}</p>
+      <p class="hot-city-en-description">{{ activeSwiper.enDescription }}</p>
+      <p class="hot-city-split-text">{{ activeSwiper.splitText }}</p>
+      <div class="description-wrapper">
+        <p
+          class="cn-description-item"
+          v-for="(item, i) in activeSwiper.description"
+          :key="i"
+        >
+          {{ item }}
+        </p>
+      </div>
+      <div class="swiper-dots">
+        <span
+          class="swiper-dot"
+          :class="{ active: j === activeIndex + 1 }"
+          v-for="j in hotDetail.length"
+          :key="j"
+        ></span>
+      </div>
+      <div class="hot-swiper-control">
+        <al-icon
+          name="arrowGray"
+          position="left"
+          @click="handleSwiperNext(-1)"
+        ></al-icon>
+        <al-icon
+          name="arrowGray"
+          position="right"
+          @click="handleSwiperNext(1)"
+        ></al-icon>
+      </div>
+      <div class="swiper-round-active">0{{ activeIndex + 1 }}</div>
+      <div class="swiper-item-a">
+        <al-image :src="activeSwiper.imageB"></al-image>
+      </div>
+      <div class="swiper-item-b">
+        <al-image :src="activeSwiper.imageA"></al-image>
+      </div>
+    </div>
+    <al-image class="layout-mobile-only" :src="activeSwiper.mobile"></al-image>
     <al-image
       class="layout-desktop-full"
-      src="./images/section-hot-pc.png"
+      :src="activeSwiper.desktop"
     ></al-image>
   </al-section>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      activeIndex: 0,
+      hotDetail: [
+        {
+          mobile: "./images/hot-1.png",
+          desktop: "./images/section-hot-pc.png",
+          title: "我没去东京。",
+          subtitle: "",
+          en: "Tokyo.",
+          enDescription: "but the story is never ends.",
+          splitText: "撕裂与重组",
+          description: [
+            "日本有看不惯一切的习惯，喜欢在设计上进行破坏、涂抹、剥离、撕碎、不对称剪裁；运用缠绕、悬垂、层叠等方式来处理设计。",
+            "潮流在日本当中扮演的角色,是一种生活方式。是艺术，将我们眼睛中的盲点取消，让我们发现了更多。",
+          ],
+          imageA: "./images/hot-1-a.jpg",
+          imageB: "./images/hot-1-b.jpg",
+        },
+        {
+          mobile: "./images/hot-2.png",
+          desktop: "./images/section-hot-pc.png",
+          title: "我没去巴黎。",
+          subtitle: "",
+          en: "Tokyo.",
+          enDescription: "but the story is never ends.",
+          splitText: "撕裂与重组",
+          description: [
+            "日本有看不惯一切的习惯，喜欢在设计上进行破坏、涂抹、剥离、撕碎、不对称剪裁；运用缠绕、悬垂、层叠等方式来处理设计。",
+            "潮流在日本当中扮演的角色,是一种生活方式。是艺术，将我们眼睛中的盲点取消，让我们发现了更多。",
+          ],
+          imageA: "./images/hot-2-a.jpg",
+          imageB: "./images/hot-2-b.jpg",
+        },
+        {
+          mobile: "./images/hot-3.png",
+          desktop: "./images/section-hot-pc.png",
+          title: "我没去米兰。",
+          subtitle: "",
+          en: "Tokyo.",
+          enDescription: "but the story is never ends.",
+          splitText: "撕裂与重组",
+          description: [
+            "日本有看不惯一切的习惯，喜欢在设计上进行破坏、涂抹、剥离、撕碎、不对称剪裁；运用缠绕、悬垂、层叠等方式来处理设计。",
+            "潮流在日本当中扮演的角色,是一种生活方式。是艺术，将我们眼睛中的盲点取消，让我们发现了更多。",
+          ],
+          imageA: "./images/hot-3-a.jpg",
+          imageB: "./images/hot-3-b.jpg",
+        },
+        {
+          mobile: "./images/hot-4.png",
+          desktop: "./images/section-hot-pc.png",
+          title: "我没去巴塞罗那。",
+          subtitle: "",
+          en: "Tokyo.",
+          enDescription: "but the story is never ends.",
+          splitText: "撕裂与重组",
+          description: [
+            "日本有看不惯一切的习惯，喜欢在设计上进行破坏、涂抹、剥离、撕碎、不对称剪裁；运用缠绕、悬垂、层叠等方式来处理设计。",
+            "潮流在日本当中扮演的角色,是一种生活方式。是艺术，将我们眼睛中的盲点取消，让我们发现了更多。",
+          ],
+          imageA: "./images/hot-4-a.jpg",
+          imageB: "./images/hot-4-b.jpg",
+        },
+      ],
+    };
+  },
+  computed: {
+    activeSwiper() {
+      return this.hotDetail[this.activeIndex];
+    },
+  },
+  methods: {
+    handleSwiperNext(value) {
+      let length = this.hotDetail.length;
+      let index = this.activeIndex + value;
+      if (index < 0) {
+        index = length - 1;
+      } else if (index > length - 1) {
+        index = 0;
+      }
+      this.activeIndex = index;
+    },
+  },
+};
+</script>
 <style lang="scss">
 .widget-index-section-hot {
   margin-top: vw(-96);
+  position: relative;
+  color: #fff;
+  .hot-wrapper {
+    position: absolute;
+    top: vw(96);
+    height: 100%;
+    width: 100%;
+    padding: 0 vw(90);
+  }
+  .hot-swiper-control {
+    display: flex;
+    align-items: center;
+    .component-icon {
+      width: vw(66);
+      height: vw(60);
+      opacity: 0.5;
+    }
+  }
+  .swiper-item-a {
+    position: absolute;
+    z-index: 10;
+    left: vw(90);
+    bottom: vw(260);
+    width: vw(570);
+    height: vw(320);
+    border-radius: vw(20);
+    overflow: hidden;
+  }
+  .swiper-item-b {
+    position: absolute;
+    z-index: 9;
+    right: vw(40);
+    bottom: vw(480);
+    width: vw(320);
+    height: vw(320);
+    border-radius: vw(20);
+    overflow: hidden;
+  }
+  .hot-city-en {
+    margin-top: vw(60);
+    font-size: vw(14);
+    color: #ddcdb7;
+  }
+  .hot-city-title {
+    font-size: vw(42);
+    font-weight: bold;
+    line-height: vw(62);
+  }
+  .hot-city-en-description {
+    margin-top: vw(12);
+    font-size: vw(14);
+  }
+  .hot-city-split-text {
+    position: relative;
+    margin-top: vw(60);
+    padding-left: vw(52);
+    font-size: vw(18);
+    font-weight: bold;
+    font-style: italic;
+    &::before {
+      content: "";
+      position: absolute;
+      left: 0;
+      top: vw(9);
+      width: vw(40);
+      height: 1px;
+      background-color: #fff;
+    }
+  }
+  .cn-description-item {
+    max-width: vw(570);
+    margin-top: vw(20);
+    font-size: vw(16);
+    line-height: vw(22);
+  }
+  .swiper-dots {
+    margin: vw(50) 0;
+    width: vw(100);
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  .swiper-dot {
+    width: vw(8);
+    height: vw(8);
+    border-radius: 50%;
+    border: 1px solid rgba(255, 255, 255, 0.3);
+    &.active {
+      background-color: #fff;
+      border-color: #fff;
+    }
+  }
+  .swiper-round-active {
+    position: absolute;
+    top: vw(62);
+    right: vw(90);
+    width: vw(64);
+    height: vw(64);
+    color: #fff;
+    font-size: vw(14);
+    text-align: center;
+    line-height: vw(64);
+    border: 1px solid rgba(255, 255, 255, 0.5);
+    border-radius: 50%;
+    overflow: hidden;
+  }
+
   @include layout-desktop-full {
     margin-top: px2vw(-178);
   }
