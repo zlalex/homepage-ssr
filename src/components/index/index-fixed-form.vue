3a51e04b9f76c313e11f5ff585628a8157006341
Alex <zhouliang106@qq.com>
2021-02-09 14:23:56 +0800

    update

---
+++ src/components/index/index-fixed-form.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="component-index-fixed-form"></div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss">
+.component-index-fixed-form {
+  position: fixed;
+  z-index: 50;
+}
+</style>
